01857edcf2f0284bc80422c58d2f9a9c174bb1cd
kei5665 <kt10195665@gmail.com>
2025-07-11 12:32:42 +0900

    basepathを変更

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,17 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';   // ← 追加
+
 const nextConfig = {
+  basePath,                 // ← 追加
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.microcms-assets.io',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
+      { protocol: 'https', hostname: 'images.microcms-assets.io' },
+      { protocol: 'https', hostname: 'picsum.photos' },
+      { protocol: 'https', hostname: 'storage.googleapis.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      /* Cloudflare 経由の画像 */
+      { protocol: 'https', hostname: 'ridejob-cms-main.vercel.app' },
+      { protocol: 'https', hostname: 'ridejob.jp', pathname: '/media/**' },
     ],
   },
 };
